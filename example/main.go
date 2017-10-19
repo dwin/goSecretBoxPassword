d8a7ccabdbe01e5daf50c97fef1841e0c0e857e1
dwin <172265+dwin@users.noreply.github.com>
2017-10-19 15:41:05 -0400

    updated example

--- example/main.go
+++ example/main.go
@@ -6,12 +6,14 @@ import (
 	password "github.com/dwin/goSecretBoxPassword"
 )
 
-var mastPw = "masterpassword" // This should be from config file or environment variables rather than your source
+// These should be from config file or environment variables rather than your source
+var mastPw0 = "masterpassword"
+var mastPw1 = "masterpassword1"
 
 func main() {
 	userPw := "userpassword"
 	// Hash and encrypt passphrase
-	pwHash, err := password.Hash(userPw, mastPw, 0, password.ScryptParams{N: 32768, R: 16, P: 1}, password.DefaultParams)
+	pwHash, err := password.Hash(userPw, mastPw0, 0, password.ScryptParams{N: 32768, R: 16, P: 1}, password.DefaultParams)
 	if err != nil {
 		fmt.Println("Hash fail. ", err)
 	}
@@ -20,9 +22,21 @@ func main() {
 	// -------- Verify -------------
 	// Get pwHash from database and compare to user input using same masterpassword stored hash was created with
 	// Verify will return nil unless password does not match or other error occurs
-	err = password.Verify(userPw, mastPw, pwHash)
+	err = password.Verify(userPw, mastPw0, pwHash)
 	if err != nil {
 		fmt.Println("Verify fail. ", err)
 	}
 	fmt.Println("Success")
+
+	// --------- Update ------------
+	// Get pwhash from database and update using new masterpassphrase, version, and parameters
+	updated, err := password.UpdateMaster(mastPw1, mastPw0, 1, pwHash, password.DefaultParams)
+	if err != nil {
+		fmt.Println("Update fail. ", err)
+	}
+	err = password.Verify(userPw, mastPw1, updated)
+	if err != nil {
+		fmt.Println("Verify fail. ", err)
+	}
+	fmt.Println("Success verifying updated hash")
 }
